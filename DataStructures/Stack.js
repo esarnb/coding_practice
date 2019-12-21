537952a0358379ff85c7e08ec7f01779b323e2ab
esarnb <esarnb@gmail.com>
2019-12-21 12:15:32 -0500

    Stack class

---
+++ DataStructures/Stack.js
@@ -0,0 +1,15 @@
+class Stack {
+    constructor() {
+        this.head = null;
+        this.size = 0;
+    }
+}
+
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+let mystack = new Stack();
