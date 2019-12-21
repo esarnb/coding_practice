98018d98f673463e4adc7693e8be01019f2096ad
esarnb <esarnb@gmail.com>
2019-12-21 12:19:50 -0500

    Stack: push an element into the Stack

--- DataStructures/Stack.js
+++ DataStructures/Stack.js
@@ -10,20 +10,28 @@ class Stack {
         this.size = 0;
     }
 
-    push() {
+    push(data) {
+        let newNode = new Node(data)
 
+        //Set node as first element
+        if (!this.head) this.head = newNode;
+        //Push first element after new element
+        else {
+            newNode.next = this.head
+            this.head = newNode;
+        }
     }
 
     pop() {
-
+        //
     }
 
     peek() {
-
+        //
     }
 
     display() {
-        
+        //
     }
 
 }
@@ -35,4 +43,11 @@ class Node {
     }
 }
 
-let mystack = new Stack();
+let myStack = new Stack();
+
+myStack.push(1);
+myStack.push(2);
+myStack.push(3);
+myStack.push(4);
+myStack.push(5);
+
