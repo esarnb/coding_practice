67c6770d7c9f56049218c5f61ebe588babb7fa7c
esarnb <esarnb@gmail.com>
2019-12-21 12:49:33 -0500

    Added 2 more data structures, and implemented bubble sort

---
+++ Sorting/bubble.js
@@ -0,0 +1,41 @@
+/**
+ * (Math.random() * 10)       = 0 to 9
+ * ((Math.random() * 10) + 1) = 1 to 10
+ * (Math.random() * 100)      = 0 to 99
+ * (Math.random() * 101)      = 0 to 100
+ */
+
+ let list = [];
+
+for (let i = 0; i < 10; i++) {
+    list.push( parseInt((Math.random() * 100) + 1) )
+}
+
+console.log(list);
+
+
+// bubble Sort
+let fullySorted = false;
+while (!fullySorted) {
+
+    //Set fullySorted to true as a trigger.
+    fullySorted = true;
+
+    //Sweep through to find unsorted elements
+    for (let i = 0; i < list.length - 1; i++) {
+        // If the next element is greater than current, switch them
+        
+        if (list[i] > list[i+1]) {
+        
+            //Set fully sorted to false
+            fullySorted = false;
+        
+            //Switch higher with lower element
+            let buff = list[i]
+            list[i] = list[i+1];
+            list[i+1] = buff;
+        }
+    }
+}
+
+console.log(list);
