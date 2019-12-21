d0e4ae7be191e7fc3cb88e07671551f4aeafb1a2
esarnb <esarnb@gmail.com>
2019-12-21 11:17:17 -0500

    LL addNode

--- DataStructures/LinkedLists.js
+++ DataStructures/LinkedLists.js
@@ -10,4 +10,29 @@ class LinkedList {
         this.head = null
         this.size = 0
     }
+
+    addNode = (data) => {
+        let newNode = new Node(data); //Create a new node to insert
+        
+        // If there are no nodes in the list, add the first node
+        if (!this.head) { 
+            this.head = newNode;
+            this.size++;
+        }
+        // If there is a node, go to the end of the list and add the node to the end
+        else {
+            let current = this.head;  // Reference current node parent
+            while (current.next) current = current.next; 
+            current.next = newNode; 
+            this.size++; 
+        }
+    }
 }
+
+let mylist = new LinkedList()
+mylist.addNode(1);
+mylist.addNode(2);
+mylist.addNode(3);
+mylist.addNode(4);
+mylist.addNode(5);
+
