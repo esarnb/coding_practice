b1ff19da3e5996ec8050f880191620f79edb819a
esarnb <esarnb@gmail.com>
2019-12-21 14:23:09 -0500

    Selection: added sorting by selecting lowest element in list

--- Sorting/selection.js
+++ Sorting/selection.js
@@ -14,18 +14,22 @@ console.log(list);
 
 // Iterate through the list, look for the lowest element
 
-// set the lowest element to the front
+// flip the new lowest element to the lowest iteration position
 
 // Counter++ for finishing the previous. 
 // while: Stop when counter == length - 1 (last elem will be largest anyway)
 
-let lowest = 0, counter = 0;
+let lowest = 0; // The lowest element index in the list
+let counter = 0; //The index to be replaced with for the next lowest element.
+
+// Iterate through the list
 while (counter < list.length) {
     for (let i = counter; i < list.length; i++) {
         if (i == 0 || list[i] < list[lowest]) lowest = i; 
     }
 
-    list.unshift(list.splice(lowest, 1).pop())
+    let toReplace = list[counter];
+    list.splice(counter, 1, list.splice(lowest, 1, toReplace).pop())
     counter++;
     lowest = counter
 }
