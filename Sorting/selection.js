2fa662a57e295a3a26025ca5d8c6a89c95db4d55
esarnb <esarnb@gmail.com>
2019-12-21 14:50:46 -0500

    Selection: added comment

--- Sorting/selection.js
+++ Sorting/selection.js
@@ -12,25 +12,28 @@ console.log(list);
  *      put it into the front, repeat until list is fully sorted.
 */
 
-// Iterate through the list, look for the lowest element
-
-// flip the new lowest element to the lowest iteration position
-
-// Counter++ for finishing the previous. 
-// while: Stop when counter == length - 1 (last elem will be largest anyway)
-
 let lowest = 0; // The lowest element index in the list
 let counter = 0; //The index to be replaced with for the next lowest element.
 
+let toReplace = undefined; // The counter's element to switch with lowest
+let dataValue = undefined; // The lowest element to switch with counter's element
+
 // Iterate through the list
 while (counter < list.length) {
+    toReplace = list[counter]; // Current element to replace
+
+    // Find lowest element in remaining (limited by i=counter) list
     for (let i = counter; i < list.length; i++) {
         if (i == 0 || list[i] < list[lowest]) lowest = i; 
     }
 
-    let toReplace = list[counter];
-    list.splice(counter, 1, list.splice(lowest, 1, toReplace).pop())
+    // Next Lowest Element replaced by Current
+    dataValue =  list.splice(lowest, 1, toReplace).pop()
+    list.splice(counter, 1, dataValue) //Old Current Replaced by Lowest
+
+    // Move on to the next lowest selection
     counter++;
     lowest = counter
 }
+
 console.log(list);
