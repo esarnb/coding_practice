4f7246adae6f129473ac73536c1c80e6c178e7a3
esarnb <esarnb@gmail.com>
2019-12-24 16:08:45 -0500

    Re-did Selection sort, double for loop

--- Sorting/selection.js
+++ Sorting/selection.js
@@ -1,3 +1,12 @@
+/**
+ * Selection Sort (Runtime O(n^2) by for loops)
+ *      Take lowest element from the entire list, 
+ *      put it into the front, repeat until list is fully sorted.
+*/
+
+/**
+ * Input setup
+ */
 let list = [];
 
 for (let i = 0; i < 10; i++) {
@@ -7,33 +16,60 @@ for (let i = 0; i < 10; i++) {
 console.log(list);
 
 /**
- * Selection Sort (Runtime O(n^2) by for and while)
- *      Take lowest element from the entire list, 
- *      put it into the front, repeat until list is fully sorted.
-*/
+ * We will not start off with a while loop, because we will not constantly
+ * check if the list is sorted, the selections will let us sort it and
+ * we will be sure by the end it will be sorted.
+ * 
+ * So we will start with a for loop, with the length of list - 1, because
+ * the last element will always be the highest number.
+ */
 
-let lowest = 0; // The lowest element index in the list
-let counter = 0; //The index to be replaced with for the next lowest element.
+/**
+ * If this was a linked list, we would Remove and Insert elements
+ * based on node references and switch those, but since it is an array, we will use
+ * the splice property: .splice(index, amount, newElement) 
+ * 
+ * to insert a variable: .splice(4, 1, "newElem")
+ * to remove a variable: .splice(3, 1)
+ * 
+ * This way, we will replace the "current index" we are on, with the lowest
+ * element in the list in iteration.
+ */
 
-let toReplace = undefined; // The counter's element to switch with lowest
-let dataValue = undefined; // The lowest element to switch with counter's element
+ /** 
+  * We will use a for loop to iterate current index, and another for loop
+  * to find the lowest element in the list.
+ */
 
-// Iterate through the list
-while (counter < list.length) {
-    toReplace = list[counter]; // Current element to replace
 
-    // Find lowest element in remaining (limited by i=counter) list
-    for (let i = counter; i < list.length; i++) {
-        if (i == 0 || list[i] < list[lowest]) lowest = i; 
+ //Global switching element to not create multiple assigns
+ let buffer = -1;
+ let lowest = Number.MAX_SAFE_INTEGER; 
+
+ // Current element to replace the lowest element with: i
+ for (let i = 0; i < list.length - 1; i++) {
+
+    //Use list[i+1], to check against next value from current (i)
+    lowest = i+1; 
+     
+    //Finding the lowest element from the list: j
+    // j is = i because we know previous i's will be lowest already/used up.
+    for(let j = i; j < list.length; j++) {
+        // save the index, not the data element, since j changes on next iteration.
+        if (list[lowest] > list[j]) lowest = j;
     }
 
-    // Next Lowest Element replaced by Current
-    dataValue =  list.splice(lowest, 1, toReplace).pop()
-    list.splice(counter, 1, dataValue) //Old Current Replaced by Lowest
+    // (This one should be saving data element as we know the index of i)
+    buffer = list[lowest];
+    
+    // Set (list[i] =) the current element to element on lowest index
+    // Insert (splice) the current element's value to lowest's index
+    list[lowest] = list.splice(i, 1, buffer).pop()
+    
+ }
 
-    // Move on to the next lowest selection
-    counter++;
-    lowest = counter
-}
+ //Selection sort takes out lowest elements one by one and switches them
+ //with the ones in the front, iterating through to length - 1.
 
+ //Runtime O(n^2) because of double for loop
 console.log(list);
