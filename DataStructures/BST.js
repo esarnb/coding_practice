2135b4537b089081398c7cfd1678a5b54934a4a9
esarnb <esarnb@gmail.com>
2019-12-25 17:48:51 -0500

    BST error, only displays full tree parent node, not others.

--- DataStructures/BST.js
+++ DataStructures/BST.js
@@ -18,10 +18,10 @@ class BST {
         let prev = curr; // Previous parent picked
 
         // If BST empty, this is this highest parent node
-        if (!curr) this.parent = node;
+        if (!curr) this.parent = newNode;
         else {
             // While there is a node at the left/right (lower/higher) position
-            while (curr) {
+            while (curr.left || curr.right) {
 
                 prev = curr; // Record parent node before iterating into a child
             
@@ -41,13 +41,20 @@ class BST {
             // Set parent as current's parent node and Insert the new node into the current free position
             newNode.parent = prev;
             curr = newNode;
+
+            console.log(`Prev: ${prev.data} Curr: ${curr.data}`);
+            
         }
     }
 
+    // Display BST left to right
     display() {
+
         if (this.parent) {
+
             let curr = this.parent;
             console.log(curr.data);
+
             while (curr.left || curr.right) {
                 if (curr.left) curr = curr.left;
                 else curr = curr.right;
@@ -55,9 +62,23 @@ class BST {
             }
             
         }
+
         else console.log("No data");
+    }
+
+    // Display BST in sorted form
+    sorted() {
+
     }
 }
 
 
 let myBST = new BST();
+myBST.addNode(5)
+myBST.addNode(3)
+myBST.addNode(7)
+myBST.addNode(2)
+myBST.addNode(4)
+myBST.addNode(6)
+myBST.addNode(8)
+myBST.display()
