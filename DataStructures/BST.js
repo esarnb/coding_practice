d6316b74409da1ea34b7f24ba06e7dedf5a5b702
esarnb <esarnb@gmail.com>
2019-12-25 18:54:13 -0500

    BST Error: Added another node but still wont print entire list

--- DataStructures/BST.js
+++ DataStructures/BST.js
@@ -15,35 +15,14 @@ class BST {
         // Initialize element into a node
         let newNode = new Node(data);
         let curr = this.parent; // Main parent node
-        let prev = curr; // Previous parent picked
 
-        // If BST empty, this is this highest parent node
         if (!curr) this.parent = newNode;
         else {
-            // While there is a node at the left/right (lower/higher) position
-            while (curr.left || curr.right) {
-
-                prev = curr; // Record parent node before iterating into a child
-            
-                // If the newNode is < parent, choose left
-                if (newNode.data < curr.data) {
-                    curr = curr.left
-                }
-                // else choose right
-                else if (newNode.data > curr.data) {
-                    curr = curr.right
-                }
-                // else: if both are ==, void it
-
-                // repeat above until there is a spot to insert the node
-            }
-
-            // Set parent as current's parent node and Insert the new node into the current free position
-            newNode.parent = prev;
-            curr = newNode;
-
-            console.log(`Prev: ${prev.data} Curr: ${curr.data}`);
-            
+            while (curr.right) curr = curr.right;
+            if (!curr.left) curr.left = newNode;
+            else curr.right = newNode;
+            newNode.parent = curr;
+            return newNode;
         }
     }
 
