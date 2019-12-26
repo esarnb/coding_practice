d84584dc6790012de90680db42042cda43acd5e3
esarnb <esarnb@gmail.com>
2019-12-25 23:17:31 -0500

    Quad Tree constructor classes

---
+++ DataStructures/QuadTree.js
@@ -0,0 +1,25 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class QuadTree {
+    constructor(data) {
+        this.data = data;
+        this.nw = null;
+        this.ne = null;
+        this.se = null;
+        this.sw = null;
+    }
+
+    addNode() {
+
+    }
+
+    display() {
+        
+    }
+}
