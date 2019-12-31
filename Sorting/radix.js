ed8e407189fa097f037e7f15726d24f1a5af585b
esarnb <esarnb@gmail.com>
2019-12-31 11:33:14 -0500

    Added potential pseudocode for radix

--- Sorting/radix.js
+++ Sorting/radix.js
@@ -4,7 +4,7 @@
  * O(kn) Runtime
  * 
  * Sort by the last digit of the elements, then the second last, and so on.
- * Page 148
+ * Page 148 - No Pseudocode given
  */
 
  
@@ -18,3 +18,20 @@ for (let i = 0; i < 10; i++) {
 }
 
 console.log(list);
+
+// Pesudocode
+
+/**
+ * Sort by getting the last digits of each element
+ *      run a function on each element to get the last index, or, recursively check on each index of number
+ * 
+ * Then, when that iteration is over, increment the digit to check and run again
+ * 
+ * Whenever the digit increment goes higher than the digit length, skip over the index. 
+ *      This  is checking all elems in a list, so for loops and digit lengths (or digit sizes like [if x < 100 or (x < 200 && > lowestMark) ])
+ * 
+ * By the end this would sort it least to greatest.
+ */
+
+
+// Radix
